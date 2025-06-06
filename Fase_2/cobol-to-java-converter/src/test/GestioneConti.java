ace87460f26c54df785c7dea99e081871d4b74ec
Annalisa Egidi <6cetv5wx9qz6gpvel3gt1r9ipaeal9@bots.bitbucket.org>
2025-06-06 10:18:25 +0200

    identification and environment senza file secion

---
+++ Fase_2/cobol-to-java-converter/src/test/GestioneConti.java
@@ -0,0 +1,15 @@
+/**
+ * @author ANNALISA-EGIDI
+ * @date 2025-05-20
+ * Generated from COBOL program
+ */
+public class GestioneConti {
+    
+    // Program ID: GESTIONE-CONTI
+    // Environment Configuration
+    
+    public static void main(String[] args) {
+        // Main program logic goes here
+        System.out.println("GestioneConti - Generated from COBOL");
+    }
+}
